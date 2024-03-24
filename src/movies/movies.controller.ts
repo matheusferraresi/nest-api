5c5721f764b731215208285dc8eb788f04451c40
Marcos Theofilo <matheusferraresi@live.com>
2024-03-24 14:25:07 +0000

    test movie api

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  // @Get('/search')
+  // search(@Query('year') searchingYear: string) {
+  //   return `This will search for a movie with a year: ${searchingYear}`;
+  // }
+
+  @Get('/:id')
+  getOne(@Param('id') id: number): Movie {
+    return this.moviesService.getOne(id);
+  }
+
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    return this.moviesService.createMovie(movieData);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') id: number) {
+    return this.moviesService.deleteOne(id);
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') id: number, @Body() updateData: UpdateMovieDto) {
+    return this.moviesService.update(id, updateData);
+  }
+}
